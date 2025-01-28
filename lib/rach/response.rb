17fbe73eb04adcdd9ca91b4db5ab5fa271ef6d55
Roger Garcia <roginn@gmail.com>
2025-01-28 17:33:27 -0300

    allows for multiple function calls

--- lib/rach/response.rb
+++ lib/rach/response.rb
@@ -19,14 +19,19 @@ module Rach
       !tool_calls.nil? && !tool_calls.empty?
     end
 
-    def function_name
-      return nil unless function_call?
-      tool_calls.first.dig("function", "name")
+    def function_name(tool_call)
+      tool_call.dig("function", "name")
     end
 
-    def function_arguments
+    def each_tool_call
       return nil unless function_call?
-      JSON.parse(tool_calls.first.dig("function", "arguments"))
+      tool_calls.each do |tool_call|
+        yield tool_call
+      end
+    end
+
+    def function_arguments(tool_call)
+      JSON.parse(tool_call.dig("function", "arguments"))
     rescue JSON::ParserError
       raise ParseError, "Function arguments are not valid JSON"
     end
@@ -46,12 +51,15 @@ module Rach
     def on_function(function_class = nil, &block)
       return self unless function_call?
 
-      function = function_class.new
-      return self unless function.function_name == function_name
+      each_tool_call do |tool_call|
+        function = function_class.new
+        next unless function.function_name == function_name(tool_call)
 
-      args = function_arguments.transform_keys(&:to_sym)
-      function.validate_arguments!(args)
-      block.call(function, args)
+        args = function_arguments(tool_call).transform_keys(&:to_sym)
+        function.validate_arguments!(args)
+        block.call(function, args)
+      end
+
       self
     end
 
